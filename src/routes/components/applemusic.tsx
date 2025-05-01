bb9784930b78ac250bfb86eee94e10474cd6da0e
BomberFish <bomberfish@bomberfish.ca>
2025-05-01 12:52:04 -0400

    Apple Music playlist cover support

--- src/routes/components/applemusic.tsx
+++ src/routes/components/applemusic.tsx
@@ -3,69 +3,83 @@ import { AMManager } from "../../applemusic/manager";
 import { AMLibraryPlaylist, AMPlaylistResponse } from "../../applemusic/types";
 
 export const AMPlaylistDialog: Component<
-  { playlist: AMLibraryPlaylist; open: boolean; am: AMManager; }, { playlistInternal: AMPlaylistResponse | undefined; }
+    { playlist: AMLibraryPlaylist; open: boolean; am: AMManager },
+    { playlistInternal: AMPlaylistResponse | undefined }
 > = function () {
-  useChange([this.open], async () => {
-    if (this.open === true) {
-      this.playlistInternal = await this.am.getPlaylist(
-        this.playlist.id
-      );
-    }
-  });
+    useChange([this.open], async () => {
+        if (this.open === true) {
+            this.playlistInternal = await this.am.getPlaylist(this.playlist.id);
+        }
+    });
 
-  return (
-    <div>
-      <Dialog
-        open
-        bind:open={use(this.open)}
-        headline={this.playlist.attributes.name}
-        closeOnClick={false}
-      >
+    return (
         <div>
-          {$if(
-            use(this.playlistInternal, (p) => p !== undefined),
-            <ul>
-              {use(this.playlistInternal, (p) => Object.keys(
-                p?.resources["library-songs"] || []
-              ).map((key) => (
-                <li>
-                  {p?.resources["library-songs"]![key]
-                    .attributes.name}
-                </li>
-              ))
-              )}
-            </ul>
-          )}
+            <Dialog
+                open
+                bind:open={use(this.open)}
+                headline={this.playlist.attributes.name}
+                closeOnClick={false}
+            >
+                <div>
+                    {$if(
+                        use(this.playlistInternal, (p) => p !== undefined),
+                        <ul>
+                            {use(this.playlistInternal, (p) =>
+                                Object.keys(
+                                    p?.resources["library-songs"] || [],
+                                ).map((key) => (
+                                    <li>
+                                        {
+                                            p?.resources["library-songs"]![key]
+                                                .attributes.name
+                                        }
+                                    </li>
+                                )),
+                            )}
+                        </ul>,
+                    )}
+                </div>
+                <div>
+                    <Button type="filled" on:click={() => (this.open = false)}>
+                        Close
+                    </Button>
+                </div>
+            </Dialog>
         </div>
-        <div>
-          <Button type="filled" on:click={() => (this.open = false)}>
-            Close
-          </Button>
-        </div>
-      </Dialog>
-    </div>
-  );
+    );
 };
 
 export const AMPlaylistCard: Component<
-  { playlist: AMLibraryPlaylist; am: AMManager; }, { open: boolean; }
+    { playlist: AMLibraryPlaylist; am: AMManager },
+    { open: boolean; url: string }
 > = function () {
-  this.open = false;
-  return (
-    <div class="playlist-card" on:click={() => (this.open = true)}>
-      <Card type="elevated">
-        <h2>{this.playlist.attributes.name}</h2>
-        <p>
-          {this.playlist.attributes.description?.standard ??
-            "No description"}
-        </p>
-      </Card>
-      <AMPlaylistDialog
-        open
-        bind:open={use(this.open)}
-        playlist={this.playlist}
-        am={this.am}
-        bind:am={use(this.am)} />
-    </div>
-  );
+    this.open = false;
+    this.url = (this.playlist.attributes.artwork?.url || "")
+        .replace("{w}", "1024")
+        .replace("{h}", "1024");
+
+    this.css = `
+    img {
+      border-radius: 1.25rem;
+    }
+    `
+    return (
+        <div class="playlist-card" on:click={() => (this.open = true)}>
+            <Card type="elevated">
+                <img src={this.url} alt="Playlist cover" />
+                <h2>{this.playlist.attributes.name}</h2>
+                <p>
+                    {this.playlist.attributes.description?.standard ??
+                        "No description"}
+                </p>
+            </Card>
+            <AMPlaylistDialog
+                open
+                bind:open={use(this.open)}
+                playlist={this.playlist}
+                am={this.am}
+                bind:am={use(this.am)}
+            />
+        </div>
+    );
 };

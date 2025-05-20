1f5783757899e4710872138df9c65788028e2d86
BomberFish <bomberfish@bomberfish.ca>
2025-05-20 08:55:24 -0400

    dumb stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,42 @@
-import { defineConfig } from 'vite';
-import { dreamlandPlugin } from 'vite-plugin-dreamland';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import { dreamlandPlugin } from "vite-plugin-dreamland";
+import legacy from "@vitejs/plugin-legacy";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-	plugins: [dreamlandPlugin(), viteStaticCopy({
-		targets: [
-			{ src: "node_modules/@mercuryworkshop/epoxy-tls/minimal/epoxy.wasm", dest: "epoxy" },
-		]
-	})],
-	base: './',
-	server: {
-		port: 4090
-	}
+    plugins: [
+        dreamlandPlugin(),
+        legacy({
+            targets: [
+                "fully supports flexbox",
+                "partially supports css-grid",
+                "supports proxy",
+                "not dead",
+                "BlackBerry 10",
+                "Firefox ESR",
+            ],
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "node_modules/@mercuryworkshop/epoxy-tls/minimal/epoxy.wasm",
+                    dest: "epoxy",
+                },
+            ],
+        }),
+    ],
+    base: "./",
+    build: {
+        sourcemap: true,
+        cssMinify: "lightningcss",
+        minify: "terser",
+    },
+    css: {
+        devSourcemap: true,
+    },
+    server: {
+        host: true,
+        cors: false,
+        port: 4090,
+    },
 });
